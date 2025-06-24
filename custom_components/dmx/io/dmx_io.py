ce7579027302935b7997feb8b48443476c4aa47b
Breina <info@breina.be>
2025-06-24 19:53:59 +0200

    Remove TODO

--- custom_components/dmx/io/dmx_io.py
+++ custom_components/dmx/io/dmx_io.py
@@ -6,7 +6,6 @@ from custom_components.dmx.server.artnet_server import ArtNetServer
 
 
 class DmxUniverse:
-    # TODO #74 add switch to toggle sending output
 
     def __init__(self, port_address: PortAddress, controller: ArtNetServer, use_partial_universe: bool = True):
         self.port_address = port_address
